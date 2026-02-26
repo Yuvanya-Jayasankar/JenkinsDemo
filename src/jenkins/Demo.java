254893febd87ba4a67ca5886589d5eb1bffdd40d
Yuvanya Jayasankar <yuvanyajayasankar@gmail.com>
2026-02-26 16:09:48 +0530

    Update Demo.java

--- src/jenkins/Demo.java
+++ src/jenkins/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hello from Jenkins + GitHub");
-		System.out.println("Github-Webhook");
+		//System.out.println("Github-Webhook");
 		//for testing webhook
 
 	}
